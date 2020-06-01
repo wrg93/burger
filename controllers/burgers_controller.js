14f50abbdf47f7cf71f273c77cd5ee7d6dfce1f2
William Grundler <wrgrundler@gmail.com>
2020-05-31 19:46:37 -0700

    index.handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 //Import the model (burger.js) to use its database functions
-var cat = require("../models/burgers.js");
+var burger = require("../models/burgers.js");
 
 //Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res){
@@ -32,8 +32,8 @@ router.put("/api/burgers/:id", function(req,res) {
 
     console.log("condition", condition);
 
-    cat.update({
-        ready: req.body.sleepy
+    burger.update({
+        ready: req.body.ready
     }, condition, function(result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
@@ -47,7 +47,7 @@ router.put("/api/burgers/:id", function(req,res) {
 router.delete("/api/burgers/:id", function (req,res){
     var condition = "id = " + req.params.id;
 
-    cat.delete(condition, function(result) {
+    burger.delete(condition, function(result) {
         if (result.affectedRows == 0) {
             // If no rows were changed, then the Id must not exist, so 404
             return res.status(404).end();
