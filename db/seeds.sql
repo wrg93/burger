7bc170f39e6bc303bfd7236e1cfda5916b8094e7
William Grundler <wrgrundler@gmail.com>
2020-05-31 20:35:14 -0700

    seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('BIG BOI BURGER');
+INSERT INTO burgers (burger_name) VALUES ('SLOPPY JOSEPH');
+INSERT INTO burgers (burger_name, devoured) VALUES ('FOUR POUNDER' true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('GREASE BURGER', true);
