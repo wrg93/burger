7787f813fe803d3ebd27ff7e1933f6c3133936f4
William Grundler <wrgrundler@gmail.com>
2020-05-31 20:45:30 -0700

    fixed sql error

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers
-()
+(
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(200) NOT NULL,
     devoured BOOLEAN DEFAULT false,
