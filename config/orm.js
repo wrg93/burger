16a4e741b8e8c9cd8205b339ba497c10098644a3
William Grundler <wrgrundler@gmail.com>
2020-05-31 20:11:52 -0700

    orm

---
+++ config/orm.js
@@ -0,0 +1,68 @@
+//Import MySQL connection.
+var connection = require("../config/connection.js")
+
+//Helper functionfor SQL syntax
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i=0; i< num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+//Helper function to convert object key/value pairs to SQL snytax
+function objToSql(ob) {
+    var arr = [];
+
+    //loop through the key and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        //check to skop hidden properties
+        if (Object.hasOwnProperty.call(ob,key)) {
+            // if string with spaces, add quotations
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    //translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+//Object for all our SQL statement functions.
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connectionquery(queryString,function(err,result) {
+            if (err) {
+                throw err;
+            }cb(result);
+        });
+    },
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+//Export the orm object for the model (burger.js)
